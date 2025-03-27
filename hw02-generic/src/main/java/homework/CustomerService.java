37c9f96406514982eb09c1f341833d060876d6b8
Ремизова Юлия Анатольевна <YRemizova@alfabank.ru>
2025-03-27 23:23:52 +0300

    homework 3 reflection исправила замечания

--- hw02-generic/src/main/java/homework/CustomerService.java
+++ hw02-generic/src/main/java/homework/CustomerService.java
@@ -4,36 +4,30 @@ import java.util.*;
 
 public class CustomerService {
 
-    private final NavigableMap<Customer, String> customerService = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
+    private final NavigableMap<Customer, String> customers = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
 
     public Map.Entry<Customer, String> getSmallest() {
-        Set<Map.Entry<Customer, String>> customerSet = customerService.entrySet();
-        for (Map.Entry<Customer, String> it : customerSet)
-        {
-            var smallestCustomer = it.getKey();
-            Customer copyCustomer = new Customer(
-                    smallestCustomer.getId(),
-                    smallestCustomer.getName(),
-                    smallestCustomer.getScores());
-
-            return new java.util.AbstractMap.SimpleEntry<Customer, String>(copyCustomer, it.getValue());
+        var smallCustomer = customers.firstEntry().getKey();
+        Customer copyCustomer = new Customer(
+                smallCustomer.getId(),
+                smallCustomer.getName(),
+                smallCustomer.getScores());
+            return new java.util.AbstractMap.SimpleEntry<Customer, String>(copyCustomer, customers.firstEntry().getValue());
         }
-        return null;
-    }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        if (customerService.higherEntry(customer) != null) {
-            var nextCustomer = customerService.higherEntry(customer).getKey();
+        if (customers.higherEntry(customer) != null) {
+            var nextCustomer = customers.higherEntry(customer).getKey();
             Customer copyCustomer = new Customer(
                     nextCustomer.getId(),
                     nextCustomer.getName(),
                     nextCustomer.getScores());
-            return new java.util.AbstractMap.SimpleEntry<Customer, String>(copyCustomer, customerService.higherEntry(customer).getValue());
+            return new java.util.AbstractMap.SimpleEntry<Customer, String>(copyCustomer, customers.higherEntry(customer).getValue());
         }
         return null;
     }
 
     public void add(Customer customer, String data) {
-        customerService.put(customer, data);
+        customers.put(customer, data);
     }
 }
